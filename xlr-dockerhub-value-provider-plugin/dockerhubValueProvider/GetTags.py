6c87156ec8e7552741bfd5d41e4656f1ded8f548
José Carlos <josecarlos.lopezayala@gmail.com>
2020-04-13 13:19:21 +0200

    first commit

---
+++ xlr-dockerhub-value-provider-plugin/dockerhubValueProvider/GetTags.py
@@ -0,0 +1,16 @@
+import urllib2
+import urllib
+import json
+
+url = valueProvider.dockerhubServer.url
+auth = urllib.urlencode({'username': valueProvider.dockerhubServer.username, 'password': valueProvider.dockerhubServer.password})
+request = urllib2.Request(url + "/v2/users/login", auth)
+response = urllib2.urlopen(request)
+data = json.load(response)
+request = urllib2.Request(url + "/v2/repositories/" + valueProvider.dockerhubServer.username + "/" + releaseVariables['images'] + "/tags/?page_size=100")
+request.add_header("Authorization", "JWT %s" % data['token'])
+response = urllib2.urlopen(request)
+data = json.load(response)
+result = []
+for i in data['results']:
+    result = result + [i['name']]
